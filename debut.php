3aadb08135c55024387756165bbcda066f885ccc
loik froelich <loikfr@live.fr>
2020-04-16 15:28:42 +0200

    Modif de début pour pas de bug

--- debut.php
+++ debut.php
@@ -3,21 +3,26 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <form action="fin.php" method="post">
+        <form action="inscription.php" method="post">
             <div>
                 <label for="name">Nom :</label>
 	           </br>
-                <input type="text" id="name" name="user_name" placeholder="Entrez votre nom">
+                <input type="text" id="name" name="nom" placeholder="Entrez votre nom">
             </div>
 	       <div>
                 <label for="prenom">Prenom :</label>
 	               </br>
-                <input type="text" id="prenom" name="user_prenom" placeholder="Entrez votre prenom">
+                <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prenom">
             </div>
             <div>
                 <label for="mail">Mail :</label>
 	               </br>
-                <input type="text" id="mail" name="user_mail" placeholder="Entrez votre mail">
+                <input type="text" id="mail" name="mail" placeholder="Entrez votre mail">
+            </div>
+            <div>
+                <label for="mail">Genre :</label>
+	               </br>
+                <input type="text" id="genre" name="genre" placeholder="Entrez votre genre">
             </div>
             <div class="button">
                 <button type="submit">Validez</button>
